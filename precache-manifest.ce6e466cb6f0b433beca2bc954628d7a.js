3b9a5b47e1d24a90935eb13bf193bdd66448c61a
FujiHaruka <fizzyjohn1427@gmail.com>
2020-07-04 15:16:52 +0900

    Updates

---
+++ precache-manifest.ce6e466cb6f0b433beca2bc954628d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7991a0fe2b35210b3b2f854bd97d844",
+    "url": "/color-rotation/index.html"
+  },
+  {
+    "revision": "785c7a0792836e1e2e41",
+    "url": "/color-rotation/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "be18f58cf88bc32ede45",
+    "url": "/color-rotation/static/js/2.4f5ee551.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/color-rotation/static/js/2.4f5ee551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785c7a0792836e1e2e41",
+    "url": "/color-rotation/static/js/main.490697ce.chunk.js"
+  },
+  {
+    "revision": "7001c9e666c30dbe46e7",
+    "url": "/color-rotation/static/js/runtime-main.dea2a724.js"
+  }
+]);
