eeb7c1183ba6a0a18b80b08c21fbb0f33ad092d5
FujiHaruka <fizzyjohn1427@gmail.com>
2020-07-04 14:56:32 +0900

    Improve

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,49 @@ import {
   createMuiTheme,
   ThemeProvider,
 } from "@material-ui/core/styles";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 import { grey } from "@material-ui/core/colors";
 import Avatar from "@material-ui/core/Avatar";
 import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography"
-import Badge from '@material-ui/core/Badge';
-import Tooltip from '@material-ui/core/Tooltip';
-import WarningIcon from "@material-ui/icons/Warning"
+import Typography from "@material-ui/core/Typography";
+import Badge from "@material-ui/core/Badge";
+import Tooltip from "@material-ui/core/Tooltip";
+import WarningIcon from "@material-ui/icons/Warning";
 import * as convert from "@csstools/convert-colors";
+import Color from "color";
 
-const DEFAULT_COLOR = "#f28561";
+type ColorArray = [number, number, number];
+
+const css2rgb = (css: string) =>
+  Color(css)
+    .rgb()
+    .array()
+    .map((c) => (c / 255) * 100) as ColorArray;
+const rgbValue = (c: number) => Math.ceil((c / 100) * 255);
+const rgbString = ([r, g, b]: number[]) =>
+  `rgb(${rgbValue(r)},${rgbValue(g)},${rgbValue(b)})`;
+
+const DEFAULT_COLOR = css2rgb("#f28561");
 const DEFAULT_COUNT = 12;
 
-const rgbValue = (c: number) => Math.ceil(c / 100 * 255)
-const rgbString = ([r, g, b]: number[]) => `rgb(${rgbValue(r)},${rgbValue(g)},${rgbValue(b)})`
-
-const rotateLCHColor = (base: string, degree: number) => {
-  const [l, c, h] = convert.hex2lch(base)
-  const actual = convert.lch2rgb(l, c, h + degree)
-  const [r, g, b] = actual.map((c) => Math.max(0, Math.min(100, c)))
+const rotateLCHColor = (base: ColorArray, degree: number) => {
+  const [l, c, h] = convert.rgb2lch(...base);
+  const actual = convert.lch2rgb(l, c, h + degree);
+  const [r, g, b] = actual.map((c) => Math.max(0, Math.min(100, c)));
   return {
     rgb: rgbString([r, g, b]),
     actual: rgbString(actual),
-  }
-}
+  };
+};
 
-const rotateHSLColor = (base: string, degree: number) => {
-  const hsl = convert.hex2hsl(base)
-  const hue = (hsl[0] + degree) % 360
-  const rgb = convert.hsl2rgb(hue, hsl[1], hsl[2])
+const rotateHSLColor = (base: ColorArray, degree: number) => {
+  const hsl = convert.rgb2hsl(...base);
+  const hue = (hsl[0] + degree) % 360;
+  const rgb = convert.hsl2rgb(hue, hsl[1], hsl[2]);
   return {
-    rgb:  rgbString(rgb)
-  }
-}
+    rgb: rgbString(rgb),
+  };
+};
 
 const theme = createMuiTheme({
   palette: {
@@ -60,11 +69,11 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     marginTop: theme.spacing(2),
-    padding:theme.spacing(2),
+    padding: theme.spacing(2),
   },
   colorTitle: {
-    textAlign: 'center',
-    marginTop: theme.spacing(4)
+    textAlign: "center",
+    marginTop: theme.spacing(4),
   },
   colors: {
     display: "flex",
@@ -76,80 +85,118 @@ const useStyles = makeStyles((theme) => ({
   ball: {
     width: theme.spacing(11),
     height: theme.spacing(11),
-    fontSize: '2.5em'
+    fontSize: "2.5em",
   },
-  ballTooltip: {
-
-  },
+  ballTooltip: {},
   ballRoot: {
-    textAlign: 'center'
+    textAlign: "center",
   },
   ballCaption: {
-    display: 'block'
-  }
+    display: "block",
+    maxWidth: "6em",
+  },
+  footer: {
+    marginTop: theme.spacing(12),
+  },
 }));
 
 const ColorInput: FC<{
-  color: string;
-  onChangeColor: (color: string) => void;
-}> = ({ color, onChangeColor }) => {
-  const [value, setValue] = useState(DEFAULT_COLOR);
+  onChangeColor: (color: ColorArray) => void;
+}> = ({ onChangeColor }) => {
+  const [value, setValue] = useState("#f28561");
+  const [error, setError] = useState(false);
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const next = event.target.value;
     setValue(next);
-    if (convert.hex2rgb(next)) {
-      onChangeColor(next);
+    try {
+      const rgb = css2rgb(next);
+      onChangeColor(rgb);
+      if (error) {
+        setError(false);
+      }
+    } catch (e) {
+      if (!error) {
+        setError(true);
+      }
     }
   };
   return (
     <TextField
-      label="Standard color (hex)"
-      error={value !== color}
+      label="Standard color"
+      error={error}
       value={value}
       onChange={onChange}
     />
   );
 };
 
-const CountInput: FC<{ count: number, onChangeCount: (count: number) => void }> = ({ count, onChangeCount }) => {
-  const [ value, setValue ] = useState(String(DEFAULT_COUNT))
+const CountInput: FC<{
+  count: number;
+  onChangeCount: (count: number) => void;
+}> = ({ count, onChangeCount }) => {
+  const [value, setValue] = useState(String(DEFAULT_COUNT));
+  const [error, setError] = useState(false);
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const next = event.target.value;
     setValue(next);
-    const num = Number(next)
-    if (Number.isInteger(num) && num > 0) {
+    const num = Number(next);
+    if (Number.isInteger(num) && num > 0 && num < 101) {
       onChangeCount(Number(next));
+      if (error) {
+        setError(false);
+      }
+    } else {
+      if (!error) {
+        setError(true);
+      }
     }
   };
   return (
     <TextField
+      error={error}
       label="Count"
       type="number"
       value={value}
       onChange={onChange}
     />
   );
-}
+};
 
-const ColorBall: FC<{ color: string, actual?: string }> = ({ color, actual }) => {
+const ColorBall: FC<{ color: string; actual?: string }> = ({
+  color,
+  actual,
+}) => {
   const classes = useStyles();
   return (
-    <div className={classes.ballRoot} >
-    <Badge badgeContent={
-      actual && actual !== color &&
-      <Tooltip title={`Actual color is ${actual}`}>
-        <WarningIcon fontSize="small" color="primary" />
-      </Tooltip>
-    }>
-    <Avatar className={classes.ball} style={{ backgroundColor: color }}>
-      #
-    </Avatar>
-    </Badge>
-    <Typography variant="caption" className={classes.ballCaption} style={{ color }}>
-      {color}
-    </Typography>
+    <div className={classes.ballRoot}>
+      <Badge
+        badgeContent={
+          actual &&
+          actual !== color && (
+            <Tooltip title={`Actual color is ${actual}`}>
+              <WarningIcon fontSize="small" color="primary" />
+            </Tooltip>
+          )
+        }
+      >
+        <Avatar className={classes.ball} style={{ backgroundColor: color }}>
+          #
+        </Avatar>
+      </Badge>
+      <Typography
+        variant="caption"
+        className={classes.ballCaption}
+        style={{ color }}
+      >
+        {color}
+      </Typography>
     </div>
   );
+};
+
+const Footer = () => {
+  const classes = useStyles();
+  return <div className={classes.footer}></div>;
 };
 
 const App: FC = () => {
@@ -159,32 +206,33 @@ const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <Container maxWidth="md">
-      <Paper variant="outlined" className={classes.paper}>
-        <header className={classes.header}>
-          <h1>Hue rotation: HCL vs LHC</h1>
-        </header>
-        <form className={classes.form} noValidate autoComplete="off">
-          <ColorInput color={color} onChangeColor={setColor} />
-          <CountInput count={count} onChangeCount={setCount} />
-        </form>
-      </Paper>
-          <h2 className={classes.colorTitle}>Hue rotation in LCH color space</h2>
+        <Paper variant="outlined" className={classes.paper}>
+          <header className={classes.header}>
+            <h1>Hue rotation: HCL vs LHC</h1>
+          </header>
+          <form className={classes.form} noValidate autoComplete="off">
+            <ColorInput onChangeColor={setColor} />
+            <CountInput count={count} onChangeCount={setCount} />
+          </form>
+        </Paper>
+        <h2 className={classes.colorTitle}>Hue rotation in LCH color space</h2>
         <div className={classes.colors}>
           {Array.from({ length: count }).map((_, i) => {
-            const degree = (360 / count) * i
-            const { rgb, actual } = rotateLCHColor(color, degree)
-            return <ColorBall color={rgb} actual={actual} key={i} />
+            const degree = (360 / count) * i;
+            const { rgb, actual } = rotateLCHColor(color, degree);
+            return <ColorBall color={rgb} actual={actual} key={i} />;
           })}
         </div>
-          <h2 className={classes.colorTitle}>Hue rotation in HSL color space</h2>
+        <h2 className={classes.colorTitle}>Hue rotation in HSL color space</h2>
         <div className={classes.colors}>
           {Array.from({ length: count }).map((_, i) => {
-            const degree = (360 / count) * i
-            const { rgb } = rotateHSLColor(color, degree)
-            return <ColorBall color={rgb} key={i} />
+            const degree = (360 / count) * i;
+            const { rgb } = rotateHSLColor(color, degree);
+            return <ColorBall color={rgb} key={i} />;
           })}
         </div>
       </Container>
+      <Footer />
     </ThemeProvider>
   );
 };
--- src/convert-colors.d.ts
+++ src/convert-colors.d.ts
@@ -3,8 +3,26 @@ declare module "@csstools/convert-colors" {
   export const hex2lch: (hex: string) => [number, number, number];
   export const hex2hsl: (hex: string) => [number, number, number];
   export const hsl2hex: (h: number, s: number, l: number) => string;
-  export const hsl2rgb: (h: number, s: number, l: number) => [number, number, number];
+  export const hsl2rgb: (
+    h: number,
+    s: number,
+    l: number
+  ) => [number, number, number];
   export const lch2hex: (l: number, c: number, h: number) => string;
-  export const lch2rgb: (l: number, c: number, h: number) => [number, number, number];
+  export const lch2rgb: (
+    l: number,
+    c: number,
+    h: number
+  ) => [number, number, number];
   export const rgb2hex: (r: number, g: number, b: number) => string;
+  export const rgb2hsl: (
+    r: number,
+    g: number,
+    b: number
+  ) => [number, number, number];
+  export const rgb2lch: (
+    r: number,
+    g: number,
+    b: number
+  ) => [number, number, number];
 }
